2649e624524eaa8e8bff85e85424d8465f3b832a
Amogelang Matlhaga <ST10355816@rcconnect.edu.za>
2025-10-23 19:45:23 +0200

    added customised messages for the login page

--- Models/ErrorViewModel.cs
+++ Models/ErrorViewModel.cs
@@ -2,6 +2,7 @@ namespace part1_poe.Models
 {
     public class ErrorViewModel
     {
+
         public string? RequestId { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
--- Models/login.cs
+++ Models/login.cs
@@ -4,6 +4,8 @@ namespace part1_poe.Models
 {
     public class login
     {
+        //customised messages for the login error handling
+
         [Required(ErrorMessage = "Email is required")]
         [EmailAddress(ErrorMessage = "Enter a valid email address")]
         public string Email { get; set; }
